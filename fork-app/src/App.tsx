935b625ed5a6543d0cc0139860321f1b568b6991
Sean <seantraut@gmail.com>
2020-12-01 14:27:57 -0500

    fixed dependencies, drafted mainCTA + nav

--- fork-app/src/App.tsx
+++ fork-app/src/App.tsx
@@ -1,15 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
+      <MainCTA />
     </div>
   );
 }
 
 interface NavBarProps{}
-function NavBar(props:NavBarProps){}
+function NavBar(props:NavBarProps){
+  return(
+    <div className="NavBar">
+      <ul className="nav-list">
+        <li className="nav-li"><a className="nav-text" href="#home">Home</a></li>
+        <li className="nav-li"><a className="nav-text" href="#why">The Importance of Fact Checking</a></li>
+        <li className="nav-li"><a className="nav-text" href="#how">How to Fact Check</a></li>
+        <li className="nav-li"><a className="nav-text" href="#resources">Helpful Resources</a></li>
+      </ul>
+    </div>
+  );
+}
+
+interface MainCTAProps{}
+function MainCTA(props:MainCTAProps){
+  return(
+    <div className="MainCTA">
+      <div className="hero-image" />
+      <h1 className="page-title">The Facts of Real Knowledge</h1>
+      <div className="page-logo" />
+    </div>
+  );
+}
 
 export default App;
