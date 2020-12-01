d794b3a5131e88fdc7dfe5d9c7000d13e40f0e59
Sean Traut <seantraut@gmail.com>
2020-12-01 13:11:38 -0500

    Updated enviro for tsx

---
+++ fork-app/src/App.tsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+interface NavBarProps{}
+function NavBar(props:NavBarProps){}
+
+export default App;
