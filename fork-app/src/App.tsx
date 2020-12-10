aac7161c99f608fc50ef3ce6ce3ff777a0024cc5
Sean <seantraut@gmail.com>
2020-12-10 12:14:40 -0500

    Removed extra MainCTA definition

--- fork-app/src/App.tsx
+++ fork-app/src/App.tsx
@@ -10,16 +10,4 @@ function App() {
   );
 }
 
-interface MainCTAProps{}
-function MainCTA(props:MainCTAProps){
-  return(
-    <div className="MainCTA">
-      <div className="tri-one" />
-      <div className="tri-two" />
-      <h1 className="page-title title">The Facts <br/>of<br/> Real Knowledge</h1>
-      <div className="page-logo" />
-    </div>
-  );
-}
-
 export default App;
