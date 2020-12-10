67d69473cb34f36559a0b4bf4d6a3a00097e4763
Sean <seantraut@gmail.com>
2020-12-10 12:27:07 -0500

    Set up temporary single page routing

--- fork-app/src/App.tsx
+++ fork-app/src/App.tsx
@@ -2,12 +2,37 @@ import React from 'react';
 import './App.scss';
 import {HomePage} from './HomePage/HomePage';
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+export let app:App | undefined = undefined;
+
+export function rerender(){
+  if(app){
+    app.forceUpdate();
+  };
+}
+
+interface AppState{
+  page: string
+}
+
+class App extends React.Component<{}> {
+  state: AppState = {
+    page:"home"
+  };
+
+  render(){
+    let page;
+    if(this.state.page === "home"){
+      page = <HomePage />;
+    }else{
+      page = <div>404: Page Not Found</div>
+    }
+
+    return(
+      <div className="App">
+        {page}
+      </div>
+    );
+  }
 }
 
 export default App;
